895f2108eb3d90e97091ce3f6310ae88e0c9e8fb
Enotishka <enotishka18@gmail.com>
2022-08-31 19:25:46 +0300

    Lesson-2

--- lesson-2/src/App.jsx
+++ lesson-2/src/App.jsx
@@ -1,49 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 import "./App.css";
 
 
-class App extends React.Component {
-  constructor() {
-    super();
+function App() {
+  const [message, setMessage] = useState("");
+  const [messageList, setMessageList] = useState([]);
 
-    this.state = {
-      message: "",
-      messageList: [
-        {
-          text: "Hi",
-          author: "Julia"
-        },
-        {
-          text: "Hello",
-          author: "Dmitry"
-        }
-
-      ]
-    };
-  } 
-
-  render() {
-    return <div className="app">
-      <div className="chat content">
-        <div className="messages">{this.state.messageList.map((message, i) => <div key={i}>{message.author}: {message.text}</div>)}</div>
-        <div className="footer">
-          <input onChange={e => this.setState({message: e.target.value})} value={this.state.message} type="text"></input>
-          <button onClick={this.handleSend.bind(this)}>Send</button>
-        </div> 
-      </div>
-    </div>;
-  }
-
-  handleSend() {
-    const {messageList} = this.state;
-    messageList.push({
-      text: this.state.message,
-      author: "Julia",
-    });
-    this.setState({messageList, message: ""});
-
-  }
+  return <div className="app">
+    <div className="chat content">
+      <div className="messages">{messageList.map((m, i) => <div key={i}>{m.author}: {m.text}</div>)}</div>
+      <div className="footer">
+        <input onChange={e => setMessage(e.target.value)} value={message} type="text"></input>
+        <button onClick={() => {
+          const list = [...messageList];
+          list.push({
+            text: message,
+            author: "Julia",
+          });
+          setMessageList(list);
+          setMessage("");
+        }}>Send</button>
+      </div> 
+    </div>
+  </div>;
 }
-
 
 export default App;
