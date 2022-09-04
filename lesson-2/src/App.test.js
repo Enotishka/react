d6a4b8fde43b63844ddd11410d094b696ac113fd
Enotishka <enotishka18@gmail.com>
2022-09-04 17:49:27 +0300

    lesson-3 solition

--- lesson-2/src/App.test.js
+++ lesson-2/src/App.test.js
@@ -1,8 +1,41 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen, fireEvent, within } from "@testing-library/react";
+import { App } from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("renders component", () => {
+  const { container } = render(<App />);
+
+  expect(container.getElementsByClassName("messages").length).toBe(1);
+  expect(container.getElementsByClassName("message").length).toBe(1);
+  expect(container.getElementsByClassName("btn-send").length).toBe(1);
+});
+
+test("user sends meessage", () => {
+  const { container } = render(<App />);
+
+  const input = container.getElementsByClassName("message")[0];
+  fireEvent.change(input, { target: { value: "test message" } });
+  const button = container.getElementsByClassName("btn-send")[0];
+  fireEvent.click(button);
+  const messages = container.getElementsByClassName("messages")[0];
+  expect(
+    within(messages).queryByText("Julia: test message")
+  ).toBeInTheDocument();
+});
+
+test("bot sends meessage", () => {
+  const { container } = render(<App />);
+
+  const input = container.getElementsByClassName("message")[0];
+  fireEvent.change(input, { target: { value: "test message" } });
+  const button = container.getElementsByClassName("btn-send")[0];
+  fireEvent.click(button);
+  const messages = container.getElementsByClassName("messages")[0];
+
+  setTimeout(
+    () =>
+      expect(
+        within(messages).queryByText("chatBot: (-_-)")
+      ).toBeInTheDocument(),
+    1600
+  );
 });
